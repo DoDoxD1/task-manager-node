e50fd03362dfbeb3947b15cbd7a98ad1151a490f
arihant <aunujain53@gmail.com>
2024-03-12 16:32:52 +0530

    final commit

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks.js');
 require('dotenv').config();
+const notFound = require('./middleware/not-found.js');
+const asyncWrapper = require('./middleware/async.js');
 
 app.use(express.json());
 app.use(express.static('./public'));
 //routes
 
-app.get('/hello', (req, res) => {
-  res.send('Helllo');
-});
-
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
